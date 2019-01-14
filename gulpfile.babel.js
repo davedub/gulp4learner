304722cc302e3093086b659206230abd8104ceb0
davedub <dwitus@gmail.com>
2019-01-14 11:03:33 -0800

    use import statements in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,15 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
+const gulp = require('gulp');
 const babel = require('gulp-babel');
-const webpackconfig = require("./webpack.config.js");
-const webpackstream = require("webpack-stream");
-const babelregister = require("@babel/register");
+const webpackconfig = require('./webpack.config.js');
+const webpackstream = require('webpack-stream');
+const babelregister = require('@babel/register');
 
-var css = require('./build-tasks/styles');
-var messages = require('./build-tasks/messages');
-var sayhey = require('./build-tasks/messages');
-var webpack = require('./build-tasks/webpack');
-
-// import [ sayhey, messages ] from './build-tasks/messages'
-// import css from './build-tasks/styles'
-// import index from './build-tasks/index'
-// import webpack from './build-tasks/webpack'
+import {
+    sayhey,
+    messages
+} from './build-tasks/messages';
+import css from './build-tasks/styles';
+import webpack from './build-tasks/webpack';
+import index from './build-tasks/index';
